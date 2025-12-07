41ac8323d5cce9506864447d2af300afa919bf21
Kpri <kirill.priyatnov@gmail.com>
2025-12-07 16:24:57 +0300

    hw 2 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,42 +10,72 @@ var nums = map[rune]int{'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, 
 
 func Unpack(s string) (string, error) {
 	// Place your code here.
-	unpacked := strings.Builder{}
+	unpacked := &strings.Builder{}
+	unpacked, err := iterateStringAndUnpackRunes(s, unpacked)
+	if err != nil {
+		return unpacked.String(), err
+	}
+
+	return unpacked.String(), nil
+}
+
+func iterateStringAndUnpackRunes(s string, unpacked *strings.Builder) (*strings.Builder, error) {
 	r := []rune(s)
 	for i := 0; i < len(r); i++ {
 		num, ok := nums[r[i]]
 		if ok && i > 0 {
-			prevRune := string(r[i-1])
-			isLastRune := i == len(r)-1
-			if !isLastRune {
-				nextRune := r[i+1]
-				_, ok := nums[nextRune]
-				if ok {
-					return unpacked.String(), ErrInvalidString
-				}
-			}
-			if num == 0 {
-				runeArr := []rune(unpacked.String())
-				runeArr = runeArr[:len(runeArr)-1]
-				unpacked.Reset()
-				unpacked.WriteString(string(runeArr))
-			}
-			for i := 0; i < num-1; i++ {
-				unpacked.WriteString(prevRune)
+			unpacked, err := handleNum(r, unpacked, num, i)
+			if err != nil {
+				return unpacked, err
 			}
 		} else if curRune := r[i]; curRune == '\\' {
-			nextRune := r[i+1]
-			_, ok := nums[nextRune]
-			if !ok && nextRune != '\\' {
-				return unpacked.String(), ErrInvalidString
+			unpacked, err := handleEscapeSlash(r, unpacked, i)
+			if err != nil {
+				return unpacked, err
 			}
-			unpacked.WriteRune(nextRune)
 			i++
 		} else if ok && i == 0 {
-			return unpacked.String(), ErrInvalidString
+			return unpacked, ErrInvalidString
 		} else {
 			unpacked.WriteRune(r[i])
 		}
 	}
-	return unpacked.String(), nil
+	return unpacked, nil
+}
+
+func handleNum(r []rune, unpacked *strings.Builder, num int, i int) (*strings.Builder, error) {
+	prevRune := string(r[i-1])
+	isLastRune := i == len(r)-1
+	if !isLastRune {
+		nextRune := r[i+1]
+		_, ok := nums[nextRune]
+		if ok {
+			return unpacked, ErrInvalidString
+		}
+	}
+	if num == 0 {
+		unpacked = removeLastRune(unpacked)
+	}
+	for i := 0; i < num-1; i++ {
+		unpacked.WriteString(prevRune)
+	}
+	return unpacked, nil
+}
+
+func removeLastRune(unpacked *strings.Builder) *strings.Builder {
+	runeArr := []rune(unpacked.String())
+	runeArr = runeArr[:len(runeArr)-1]
+	unpacked.Reset()
+	unpacked.WriteString(string(runeArr))
+	return unpacked
+}
+
+func handleEscapeSlash(r []rune, unpacked *strings.Builder, i int) (*strings.Builder, error) {
+	nextRune := r[i+1]
+	_, ok := nums[nextRune]
+	if !ok && nextRune != '\\' {
+		return unpacked, ErrInvalidString
+	}
+	unpacked.WriteRune(nextRune)
+	return unpacked, nil
 }
